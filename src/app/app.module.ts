5a289977aaf90a7040342ff8ccbe820bc1e40f41
AlexBGH <58218640+AlexBGH@users.noreply.github.com>
2022-05-29 14:14:01 +0200

    add components header, footer and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarvelHomeComponent } from './marvel-home/marvel-home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiceComponent } from './service/service.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarvelHomeComponent
+    MarvelHomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServiceComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
